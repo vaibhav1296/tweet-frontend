b61bf20ff0316382c00686f5e7aa9ec094a80992
vaibhav1296 <sharmavaibhav5796@gmail.com>
2022-02-20 13:50:58 +0530

    .

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {store} from './state/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
